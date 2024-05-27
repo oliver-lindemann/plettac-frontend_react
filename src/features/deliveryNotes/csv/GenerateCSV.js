c5e7edfb3b5fd3e1a53730858cdd859ee18bd58d
Oliver Lindemann <lindemann.oliver@gmx.de>
2024-05-27 12:52:13 +0200

    Update CSV Generation

--- src/features/deliveryNotes/csv/GenerateCSV.js
+++ src/features/deliveryNotes/csv/GenerateCSV.js
@@ -1,17 +1,20 @@
-export const LOCATION_ID = 100200;
+export const LOCATION_ID = "K133296";
+
+const CRLF = "\r\n";
 
 export const generateCSV = (deliveryNote) => {
-  let csvFile = "data:text/csv;charset=utf-8,";
+  const csvFileString = deliveryNote?.parts?.reduce(
+    (result, partItem) =>
+      result +
+      CRLF +
+      [
+        `"${deliveryNote.customer?.customerNr}"`,
+        `"${partItem.part.articleNr}"`,
+        partItem.amount,
+        `"${LOCATION_ID}"`,
+      ].join(","),
+    `data:text/csv;charset=utf-8;header=present,Kundennr.,Artikelnr.,Menge,Lagerort`
+  );
 
-  deliveryNote?.parts?.forEach((partItem, index) => {
-    csvFile += `
-        ${deliveryNote.customer?.customerNr},
-        ${partItem.part.articleNr},
-        ${partItem.amount},
-        ${"K133296"}
-        \r\n
-    `;
-  });
-
-  return csvFile;
+  return csvFileString;
 };
